6398a6664c57f2ae367cb2547e40da95d837db48
Debian <bitnami@ip-172-26-13-115.ec2.internal>
2020-11-14 01:23:29 +0000

    started web design stuff

--- web_stuff/fitbit.php
+++ web_stuff/fitbit.php
@@ -11,6 +11,7 @@
     <a href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22BTHZ&redirect_uri=https%3A%2F%2Fvirtualcheckup.walbers.com%2Fcallback.php&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
 Login to Fitbit
     </a>
+
 </body>
 
 </html>
