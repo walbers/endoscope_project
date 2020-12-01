8e04ec975fff78359706f8c4f5b585630e2312cc
dishen245 <32150219+dishen245@users.noreply.github.com>
2020-12-01 15:41:01 -0600

    Update arduino_test.ino

--- light_stuff/arduino_test/arduino_test.ino
+++ light_stuff/arduino_test/arduino_test.ino
@@ -1,22 +1,20 @@
 #include <SPI.h>
 
-//const int slaveSelectPin = 10;
+const int slaveSelectPin = 10;
 int keystroke = 0;
-//int value = 0;
+int value = 0;
 String thing;
-//int CS = 0;
+int CS = 0;
 
 void setup() {
 
   // set the slaveSelectPin as output:
 
-//  pinMode(slaveSelectPin, OUTPUT);
+  pinMode(slaveSelectPin, OUTPUT);
   
   // initialize SPI:
 
-//  SPI.begin();
-  pinMode(LED_BUILTIN, OUTPUT);
-
+  SPI.begin();
   Serial.begin(9600);
 }
 
@@ -25,56 +23,50 @@ void loop() {
   if (Serial.available()>0){
     thing = Serial.readString();
     keystroke = thing.toInt();
-    if (keystroke == 1) {
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      delay(1000);
+
+    Serial.print("I received: ");
+    Serial.println(keystroke);
+    Serial.println(keystroke, DEC);
+
+    if (keystroke == 0){
+      value = 0;
     }
-    if (keystroke == 2) {
-      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-      delay(1000);
+    else {
+    value = 117 + keystroke;
     }
+
   }
   
-//
-//  Serial.print("I received: ");
-//  Serial.println(keystroke);
-
-//  if (keystroke == 0){
-//    value = 0;
-//  }
-//  else {
-//    value = 117 + keystroke;
-//  }
   // go through the six channels of the digital pot:
 
-//  for (int channel = 0; channel < 1; channel++) {
-//
-//    digitalPotWrite(channel, value);
-//
-//  }
+  for (int channel = 0; channel < 1; channel++) {
+
+    digitalPotWrite(channel, value);
+
+  }
 
   delay(10);
 
 }
 
-//void digitalPotWrite(int address, int value) {
-//
-//  // take the SS pin low to select the chip:
-//
-//  digitalWrite(slaveSelectPin, LOW);
-//
-//  delay(100);
-//
-//  //  send in the address and value via SPI:
-//
-//  SPI.transfer(address);
-//
-//  SPI.transfer(value);
-//  digitalWrite(CS, LOW);
-//
-//  delay(100);
-//
-//  // take the SS pin high to de-select the chip:
-//
-//  digitalWrite(slaveSelectPin, HIGH);
-//}
+void digitalPotWrite(int address, int value) {
+
+  // take the SS pin low to select the chip:
+
+  digitalWrite(slaveSelectPin, LOW);
+
+  delay(100);
+
+  //  send in the address and value via SPI:
+
+  SPI.transfer(address);
+
+  SPI.transfer(value);
+  digitalWrite(CS, LOW);
+
+  delay(100);
+
+  // take the SS pin high to de-select the chip:
+
+  digitalWrite(slaveSelectPin, HIGH);
+}
